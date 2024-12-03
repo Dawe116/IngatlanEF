5ab81c8216f9b3eda94e8c51a5a7db45ade8e70d
Dawe116 <molnar.dawe116@gmail.com>
2024-12-03 14:45:35 +0100

    Add project files.

---
+++ IngatlanEF/Services/IngatlanService.cs
@@ -0,0 +1,51 @@
+﻿using IngatlanEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IngatlanEF.Services
+{
+    internal class IngatlanService
+    {
+        public static List<Ingatlan> GetAllIngatlans() { 
+            using(var context = new MiskolcingatlanContext()) {
+                try
+                {
+                    List<Ingatlan> result = context.Ingatlans.ToList();
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    List<Ingatlan> hibasok = new List<Ingatlan>();
+                    hibasok.Add(new Ingatlan()
+                    {
+                        Id = -1,
+                        Település = ex.Message
+                    });
+                    return hibasok;
+                }
+            }
+
+        }
+
+        public static void Insert(Ingatlan ujUngatlan)
+        {
+            using (var context = new MiskolcingatlanContext())
+            {
+                try
+                {
+                    context.Ingatlans.Add(ujUngatlan);
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                
+            }
+        }
+    }
+}
---
+++ IngatlanEF/Services/UgyintezoService.cs
@@ -0,0 +1,51 @@
+﻿using IngatlanEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngatlanEF.Services
+{
+    internal class UgyintezoService
+    {
+        public static List<Ugyintezo> GetallUgyintezo()
+        {
+            using (var context = new MiskolcingatlanContext())
+            {
+                try
+                {
+                    List<Ugyintezo> result = context.Ugyintezos.ToList();
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    List<Ugyintezo> hibasok = new List<Ugyintezo>();
+                    hibasok.Add(new Ugyintezo()
+                    {
+                        Id = -1,
+                        Nev = ex.Message
+                    });
+                    return hibasok;
+                }
+            }
+        }
+
+        public static void Insert(Ugyintezo ujUgyintezo)
+        {
+            using (var context = new MiskolcingatlanContext())
+            {
+                try
+                {
+                    context.Ugyintezos.Add(ujUgyintezo);
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }
+        }
+    }
+}
