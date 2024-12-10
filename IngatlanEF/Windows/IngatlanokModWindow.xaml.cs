fa685967b518d699f0c380d6101348b20a10bda6
Dawe116 <molnar.dawe116@gmail.com>
2024-12-10 14:05:14 +0100

    Ingatlan modositasa almost Done

--- IngatlanEF/Windows/IngatlanokModWindow.xaml.cs
+++ IngatlanEF/Windows/IngatlanokModWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IngatlanEF.Models;
+using IngatlanEF.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,60 @@ namespace IngatlanEF.Windows
     /// </summary>
     public partial class IngatlanokModWindow : Window
     {
+        List<Ingatlan> ingatlanok = IngatlanService.GetAllIngatlans();
+
+        List<Ugyintezo> ugyintezos = UgyintezoService.GetallUgyintezo();
         public IngatlanokModWindow()
         {
             InitializeComponent();
+            CmbSelectFeltolt();
+            cmbTipus.ItemsSource = MainWindow.tipusok;
+            cmbTipus.SelectedIndex = 0;
+
+            foreach (Ugyintezo item in ugyintezos)
+            {
+                cmbUgyintezo.Items.Add($"{item.Id} : {item.Nev}");
+            }
+            cmbSelect.SelectedIndex = 0;
+        }
+
+        private void CmbSelectFeltolt()
+        {
+            cmbSelect.Items.Clear();
+            foreach (Ingatlan ingatlans in ingatlanok)
+            {
+                cmbSelect.Items.Add($"{ingatlans.Id} : {ingatlans.Település} : {ingatlans.Cim} : {ingatlans.Tipus} : {ingatlans.Ár}");
+            }
+        }
+
+        private void Modify(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void cmbSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //kivalasztott megkeresése
+            Ingatlan kivalasztott = ingatlanok.FirstOrDefault(i => i.Id == int.Parse(cmbSelect.SelectedItem.ToString().Split(':')[0]));
+            //adatok megjelenítése
+            if (kivalasztott != null) {
+                tbxTelepules.Text = kivalasztott.Település;
+                tbxcCím.Text = kivalasztott.Cim;
+                tbxKepNev.Text = kivalasztott.KepNev;
+                tbxAr.Text = kivalasztott.Ár.ToString();
+                cmbTipus.SelectedItem = MainWindow.tipusok.Contains(kivalasztott.Tipus) ? kivalasztott.Tipus : "ba";
+                cmbUgyintezo.SelectedItem = $"{kivalasztott.UgyintezoId} : {ugyintezos.FirstOrDefault(u => u.Id == kivalasztott.UgyintezoId).Nev}";
+
+            }
+            else
+            {
+                MessageBox.Show("Nincs kiválasztott elem");
+            }
         }
     }
 }
